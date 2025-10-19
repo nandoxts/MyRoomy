d8e6cde039caa0548b0266befefa10526f0d62c6
fernandoxts <nandoxts01@gmail.com>
2025-10-18 19:00:44 -0500

    fix: Ajust icon svg  menu admin and client

--- app/src/main/java/com/example/myroomy/dashboard/fragments/UbicacionFragment.kt
+++ app/src/main/java/com/example/myroomy/dashboard/fragments/UbicacionFragment.kt
@@ -1,4 +1,27 @@
 package com.example.myroomy.dashboard.fragments
 
-class UbicacionFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myroomy.R
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class UbicacionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_ubicacion, container, false)
+
+        val spinnerDepartamento: Spinner = view.findViewById(R.id.spinnerDepartamento)
+        val departamentos = listOf("Lima", "Cusco", "Arequipa")
+        spinnerDepartamento.adapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, departamentos)
+
+        return view
+    }
 }
